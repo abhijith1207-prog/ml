76dbd624bbabe6927255fcf4b9768587b042e9c9
Abhijith R <abhijithabi1207@gmail.com>
2026-04-13 00:40:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 st.title('🎈  sample-ml')
 
-st.write('This app builds a machine learning model')
-
-df=pd.read_csv("https://raw.githubusercontent.com/abhijith1207-prog/ml/refs/heads/master/sample-ml.csv")
-df
+st.info('This app builds a machine learning model')
+with st.expander("Data"):
+  
+  st.write("**raw data"**)
+  df=pd.read_csv("https://raw.githubusercontent.com/abhijith1207-prog/ml/refs/heads/master/sample-ml.csv")
+  df
 
