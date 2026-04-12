0bac0bd054edc8f353d738bb323ac3bbddfb1a45
Abhijith R <abhijithabi1207@gmail.com>
2026-04-13 00:36:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈  sample-ml')
 
 st.write('This app builds a machine learning model')
+
+df=pd.read_csv("https://raw.githubusercontent.com/abhijith1207-prog/ml/refs/heads/master/sample-ml.csv")
+df
+
