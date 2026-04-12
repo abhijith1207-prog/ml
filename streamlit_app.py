4bc9a9bd52aa0c8bb9503d1628140338c8715495
Abhijith R <abhijithabi1207@gmail.com>
2026-04-13 01:27:01 +0530

    Add scatter chart for data visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,6 @@ with st.expander("Data", expanded=True):
     Y=df.species
     Y
 
+with st.expander("Data visualisation")
+    st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
+
