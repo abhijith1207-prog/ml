e629692bde433f6e058bf5bb4a0d2d81224120ab
Abhijith R <abhijithabi1207@gmail.com>
2026-04-13 01:29:08 +0530

    Fix expander syntax and update visualization label

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('🎈 sample-ml')
 
 st.info('This app builds a machine learning model')
 
-with st.expander("Data", expanded=True):
+with st.expander("Data"):
     st.write("**raw data**")
     df = pd.read_csv('https://raw.githubusercontent.com/abhijith1207-prog/ml/refs/heads/master/sample-ml.csv')
     st.dataframe(df)
@@ -18,6 +18,6 @@ with st.expander("Data", expanded=True):
     Y=df.species
     Y
 
-with st.expander("Data visualisation")
+with st.expander("Data visualization", expanded=True)
     st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
 
