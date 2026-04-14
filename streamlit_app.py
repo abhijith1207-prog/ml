f6bfcbac7fc26a442ee4cc8f18a4e00c6ab70aaa
Abhijith R <abhijithabi1207@gmail.com>
2026-04-15 00:10:32 +0530

    Fix get_dummies to use columns parameter

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ with st.expander('Input features'):
 
 #Encode 
 encode=['island','gender']
-df_penguins=pd.get_dummies(input_penguins,prefix=encode)
+df_penguins=pd.get_dummies(input_penguins,columns = ['island','gender'] )
 df_penguins[:1]
 
 
